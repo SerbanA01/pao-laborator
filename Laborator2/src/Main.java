7651497e97b64faa4168cd1b8fd5a21f13b6cf88
SerbanA01 <andrei.serban2@s.unibuc.ro>
2024-03-05 17:41:07 +0200

    Create Main.java

--- Laborator2/src/Main.java
+++ Laborator2/src/Main.java
@@ -10,8 +10,9 @@ Exemplu de secventa Fibonacci 1 2 3 5 8
 Cum se formeaza secventa Fibonacci:
 1 2 (suma anterioarelor doua numere) ...
 * */
-import java.util.Scanner;
 
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,38 +23,38 @@ public class Main {
         cerinta = cerintaScanner.nextInt();
         //1
         if(cerinta == 1){
-        for(int i = 1;i <= 99;i ++){
-            if(i % 3 == 0){
-                System.out.println(i);
+            for(int i = 1;i <= 99;i ++){
+                if(i % 3 == 0){
+                    System.out.println(i);
+                }
             }
-        }
         }
         //2
         if(cerinta == 2){
-        int a,b,c;
+            int a,b,c;
 
-        Scanner scanner = new Scanner(System.in);
-        a=scanner.nextInt();
-        b=scanner.nextInt();
-        c=scanner.nextInt();
-        if(a > b && a > c){
-            if(b > c)
-                System.out.println("a > b > c");
-            else
-                System.out.println("a > c > b");
-        }
-        else if(b > a && b > c){
-            if(a > c)
-                System.out.println("b > a > c");
-            else
-                System.out.println("b > c > a");
-        }
-        else if(c > a && c > b){
-            if(a > b)
-                System.out.println("c > a > b");
-            else
-                System.out.println("c > b > a");
-        }
+            Scanner scanner = new Scanner(System.in);
+            a=scanner.nextInt();
+            b=scanner.nextInt();
+            c=scanner.nextInt();
+            if(a > b && a > c){
+                if(b > c)
+                    System.out.println("a > b > c");
+                else
+                    System.out.println("a > c > b");
+            }
+            else if(b > a && b > c){
+                if(a > c)
+                    System.out.println("b > a > c");
+                else
+                    System.out.println("b > c > a");
+            }
+            else if(c > a && c > b){
+                if(a > b)
+                    System.out.println("c > a > b");
+                else
+                    System.out.println("c > b > a");
+            }
         }
         //3
         else{
