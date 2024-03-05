277fe61ee1e1602eb3b64932ef08ac1b715d9fd8
SerbanA01 <andrei.serban2@s.unibuc.ro>
2024-03-05 17:46:43 +0200

    Update Main.java

--- Laborator2/src/Main.java
+++ Laborator2/src/Main.java
@@ -15,6 +15,14 @@ Cum se formeaza secventa Fibonacci:
 import java.util.Scanner;
 
 public class Main {
+    public static int fibonacci(int n){
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
     public static void main(String[] args) {
 
         int cerinta;
@@ -58,6 +66,11 @@ public class Main {
         }
         //3
         else{
+
+            Scanner fibonacciScanner = new Scanner(System.in);
+            System.out.println("Introduceti numarul N: ");
+            int n = fibonacciScanner.nextInt();
+            System.out.println(fibonacci(n));
 
         }
 
